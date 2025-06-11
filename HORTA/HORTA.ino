0e5e711c1bc7955a772b0b91fbc5309b0d267791
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-06-11 15:25:46 -0300

    incremento na horta

--- HORTA/HORTA.ino
+++ HORTA/HORTA.ino
@@ -1,10 +1,14 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define PINO_SENSOR 4
+#define PINO_SENSOR D7
 OneWire barramento(PINO_SENSOR);
 DallasTemperature sensor(&barramento);
 int luminosidade;
 int umidade;
+int tempoSol=1023;
+int minutosSeco=0;
+int tempoSeco = 0;
+unsigned long millisTarefa1 = millis();
 void setup() {
   sensor.begin();
   Serial.begin(115200);
@@ -13,14 +17,27 @@ void setup() {
 void loop() {
   // Pede para o sensor fazer a leitura
   sensor.requestTemperatures();
-  
+  Serial.println("Quantas horas a planta pode ficar sem água?"); 
+  if (tempoSeco != 0){
+    tempoSeco = Serial.parseInt();
+    tempoSeco *= 60;
+  }
   umidade = analogRead(A0);
+  Serial.println(umidade);
   // Pega o valor da temperatura em Celsius
   float temperaturaC = sensor.getTempCByIndex(0);
 
   // Pega o valor da temperatura em Fahrenheit
   float temperaturaF = sensor.getTempFByIndex(0);
-
+  if (umidade > 1000 )
+  {
+    else if ((millis() - millisTarefa1) < 60000) {
+    minutosSeco ++;
+  } else if (millis() - millisTarefa1 > 60000) {
+    millisTarefa1 = millis();
+  }
+  if (minutosSeco > tempoSeco)
+  }
   // Mostra as informações no monitor serial
   Serial.print("Temperatura: ");
   Serial.print(temperaturaC);
@@ -29,9 +46,14 @@ void loop() {
   Serial.println(" °F");
   Serial.println(luminosidade);
   if (Serial.available())
+  
   {
     luminosidade = (Serial.readString().toInt());
   }
   // Espera 1 segundo para nova leitura
   delay(1000);
+}
+
+void LuzArtificial(){
+
 }
